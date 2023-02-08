b0987ce01a2a7ed0edf9c9358198cbc2f6eb5572
davidgm26 <davidgama260402@gmail.com>
2023-02-08 12:06:15 +0100

    Se crea el proyecto Spring virgen

---
+++ inmoboscoAPI/src/test/java/com/salesianostriana/dam/proyectoinmo/inmobosco/InmoboscoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.proyectoinmo.inmobosco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmoboscoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
