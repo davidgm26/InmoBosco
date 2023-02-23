398798e1f592d0a75ed85ccc6a1bd26c9be3304b
davidgm26 <davidgama260402@gmail.com>
2023-02-23 22:50:05 +0100

    Se documentan los endpoints que funcionan

--- inmobosconueva/src/main/java/com/salesianostriana/dam/proyectointerdisciplinar/inmobosco/controller/InmuebleController.java
+++ inmobosconueva/src/main/java/com/salesianostriana/dam/proyectointerdisciplinar/inmobosco/controller/InmuebleController.java
@@ -8,6 +8,12 @@ import com.salesianostriana.dam.proyectointerdisciplinar.inmobosco.model.Usuario
 import com.salesianostriana.dam.proyectointerdisciplinar.inmobosco.search.util.SearchCriteriaExtractor;
 import com.salesianostriana.dam.proyectointerdisciplinar.inmobosco.service.InmuebleService;
 import com.salesianostriana.dam.proyectointerdisciplinar.inmobosco.search.util.SearchCriteria;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -25,6 +31,72 @@ public class InmuebleController {
 
     private final InmuebleService inmuebleService;
 
+
+    @Operation(summary = "Obtiene todos los inmuebles paginados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha obtenido correctamente la lista de viviendas paginadas",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = InmuebleResponse.class),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            {
+                                                 "content": [
+                                                     {
+                                                         "tipo": "Casa",
+                                                         "descripcion": "Apartamento reformado con vistas al río. Cocina americana con electrodomÃ©sticos. Suelos de tarima. Totalmente amueblado. Zona muy tranquila.",
+                                                         "precio": 125000.0,
+                                                         "ubicacion": "Triana",
+                                                         "metrosCuadrados": 80.5,
+                                                         "numBanios": 2,
+                                                         "numHab": 1
+                                                     },
+                                                     {
+                                                         "tipo": "Piso",
+                                                         "descripcion": "Piso muy luminoso, 3 habitaciones y 2 baÃ±os. Cocina amueblada. Suelos de mÃ¡rmol. Garaje y trastero incluidos. Zona residencial tranquila.",
+                                                         "precio": 240000.0,
+                                                         "ubicacion": "Los Remedios",
+                                                         "metrosCuadrados": 120.0,
+                                                         "numBanios": 3,
+                                                         "numHab": 2
+                                                     },
+                                                     {
+                                                         "tipo": "Adosado",
+                                                         "descripcion": "Ã�tico de lujo con terraza y vistas a la ciudad. 4 habitaciones y 3 baÃ±os. Cocina completamente equipada. Suelos de madera. Aire acondicionado centralizado. Zona cÃ©ntrica y bien comunicada.",
+                                                         "precio": 340000.0,
+                                                         "ubicacion": "El Porvenir",
+                                                         "metrosCuadrados": 180.0,
+                                                         "numBanios": 4,
+                                                         "numHab": 3
+                                                     },
+                                                     {
+                                                         "tipo": "Casa",
+                                                         "descripcion": "Acogedor apartamento en zona cÃ©ntrica. Totalmente reformado y amueblado. 1 habitaciÃ³n y 1 baÃ±o. Suelos de tarima. Aire acondicionado y calefacciÃ³n.",
+                                                         "precio": 180000.0,
+                                                         "ubicacion": "San Bernardo",
+                                                         "metrosCuadrados": 90.0,
+                                                         "numBanios": 1,
+                                                         "numHab": 1
+                                                     },
+                                                     {
+                                                         "tipo": "Piso",
+                                                         "descripcion": "Piso amplio y luminoso con balcÃ³n. 3 habitaciones y 2 baÃ±os. Cocina amueblada. Suelos de terrazo. Aire acondicionado centralizado. Zona comercial y bien comunicada.",
+                                                         "precio": 295000.0,
+                                                         "ubicacion": "NerviÃ³n",
+                                                         "metrosCuadrados": 140.0,
+                                                         "numBanios": 3,
+                                                         "numHab": 2
+                                                     }
+                                                 ],
+                                            """
+                            )})}),
+            @ApiResponse(responseCode = "401",
+                    description = "Full authentication is required to access this resource",
+                    content = @Content),
+            @ApiResponse(responseCode = "403",
+                    description = "Se ha expirado el token JWT",
+                    content = @Content),
+    })
     @GetMapping("/")
     public Page<InmuebleResponse> listarTodosLosInmuebles(@RequestParam(value = "search", defaultValue = "") String search,
                                                           @PageableDefault(size = 5, page = 0) Pageable pageable) {
@@ -34,7 +106,35 @@ public class InmuebleController {
 
     }
 
-
+    @Operation(summary = "Obtiene un inmueble por su identificador")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Se ha obtenido correctamente la vivienda por su identificador",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = InmuebleResponse.class),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            {
+                                                 "tipo": "Piso",
+                                                 "descripcion": "Piso amplio y luminoso con balcón. 3 habitaciones y 2 baños. Cocina amueblada. Suelos de terrazo. Aire acondicionado centralizado. Zona comercial y bien comunicada.",
+                                                 "precio": 295000.0,
+                                                 "ubicacion": "NerviÃ³n",
+                                                 "metrosCuadrados": 140.0,
+                                                 "numBanios": 2,
+                                                 "numHab": 3
+                                             }
+                                            """
+                            )})}),
+            @ApiResponse(responseCode = "401",
+                    description = "No tienes aurorización para realizar esta petición",
+                    content = @Content),
+            @ApiResponse(responseCode = "403",
+                    description = "Se ha expirado el token JWT",
+                    content = @Content),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha encontrado el inmueble con el identificador proporcionado",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     public InmuebleResponse listarUnInmueble(@PathVariable Long id) {
         return inmuebleService.findById(id);
@@ -56,6 +156,22 @@ public class InmuebleController {
         return inmuebleService.edit(inmuebleRequest, id);
     }
 
+
+    @Operation(summary = "Elimina un inmueble por su identificador")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Se ha eliminado correctamente el inmueble seleccionado",
+                    content = @Content),
+            @ApiResponse(responseCode = "401",
+                    description = "No tienes aurorización para realizar esta petición",
+                    content = @Content),
+            @ApiResponse(responseCode = "403",
+                    description = "Se ha expirado el token JWT",
+                    content = @Content),
+            @ApiResponse(responseCode = "404",
+                    description = "No se ha encontrado el inmueble",
+                    content = @Content),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         inmuebleService.delete(id);
--- inmobosconueva/src/main/java/com/salesianostriana/dam/proyectointerdisciplinar/inmobosco/controller/UserController.java
+++ inmobosconueva/src/main/java/com/salesianostriana/dam/proyectointerdisciplinar/inmobosco/controller/UserController.java
@@ -6,6 +6,12 @@ import com.salesianostriana.dam.proyectointerdisciplinar.inmobosco.exception.Sam
 import com.salesianostriana.dam.proyectointerdisciplinar.inmobosco.model.Usuario;
 import com.salesianostriana.dam.proyectointerdisciplinar.inmobosco.security.jwt.access.JwtProvider;
 import com.salesianostriana.dam.proyectointerdisciplinar.inmobosco.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +35,49 @@ public class UserController {
     private final JwtProvider jwtProvider;
 
 
+    @Operation(summary = "creación de un nuevo usuario")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CrearUsuarioRequest.class),
+                    examples = {@ExampleObject(
+                            value = """
+                                        {
+                                            "nombre":"David",
+                                            "apellidos":"García María",
+                                            "password":"12345678",
+                                            "passwordRepeat":"12345678",
+                                            "userName": "Martinex",
+                                            "dni":"85995544X",
+                                            "edad":21,
+                                            "avatar":"https://robohash.org/81.36.52.170.png",\s
+                                            "telefono":"648627905",
+                                            "email": "vivaelsevilla@gmail.com"
+                                        }
+                                    """
+                    )})}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Se ha creado correctamente el nuevo usuario con rol de usuario",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CrearUsuarioResponse.class),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            {
+                                                "id": "ac1d8001-8680-169d-8186-8017ac5f0000",
+                                                "userName": "Martinex",
+                                                "avatar": "https://robohash.org/81.36.52.170.png",
+                                                "nombre": "David",
+                                                "apellido": "García María",
+                                                "role": "USER",
+                                                "fechaCreacion": "2023-02-23"
+                                            }
+                                                                                        """
+                            )})}),
+            @ApiResponse(responseCode = "400",
+                    description = "Ha habido algún error al intentar crear al nuevo usuario",
+                    content = @Content)
+    })
     @PostMapping("/auth/register")
     public ResponseEntity<CrearUsuarioResponse> crearUsuarioConRolUsuario(@RequestBody CrearUsuarioRequest crearUsuarioRequest) throws SameUserNameException {
 
@@ -38,6 +87,49 @@ public class UserController {
 
     }
 
+    @Operation(summary = "creación de un nuevo usuario admin")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CrearUsuarioRequest.class),
+                    examples = {@ExampleObject(
+                            value = """
+                                        {
+                                            "nombre":"David",
+                                            "apellidos":"García María",
+                                            "password":"12345678",
+                                            "passwordRepeat":"12345678",
+                                            "userName": "Martinex",
+                                            "dni":"85995544X",
+                                            "edad":21,
+                                            "avatar":"https://robohash.org/81.36.52.80.png",
+                                            "telefono":"648627905",
+                                            "email": "vivaelsevilla@gmail.com"
+                                        }
+                                    """
+                    )})}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Se ha creado correctamente el nuevo usuario con rol de administrador",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CrearUsuarioResponse.class),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            {
+                                                "id": "ac1d8001-8680-169d-8186-8017ac5f0000",
+                                                "userName": "Martinex",
+                                                "avatar": "https://robohash.org/81.36.52.80.png",
+                                                "nombre": "David",
+                                                "apellido": "García María",
+                                                "role": "Admin",
+                                                "fechaCreacion": "2023-02-23"
+                                            }
+                                                                                        """
+                            )})}),
+            @ApiResponse(responseCode = "400",
+                    description = "Ha habido algún error al intentar crear al nuevo usuario",
+                    content = @Content)
+    })
     @PostMapping("/auth/register/admin")
     public ResponseEntity<CrearUsuarioResponse> createUserWithAdminRole(@RequestBody CrearUsuarioRequest crearUsuarioRequest) throws SameUserNameException {
         Usuario user = usuarioService.crearUsuarioAdmin(crearUsuarioRequest);
@@ -45,6 +137,41 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(CrearUsuarioResponse.fromUsuario(user));
     }
 
+
+    @Operation(summary = "Endpoint para realizar el login de un usuario")
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = {@Content(mediaType = "application/json",
+                    schema = @Schema(implementation = LoginRequest.class),
+                    examples = {@ExampleObject(
+                            value = """
+                                    {
+                                        "username": "Martinex",
+                                        "password": "12345678"
+                                    }
+                                    """
+                    )})}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = JwtUserResponse.class),
+                            examples = {@ExampleObject(
+                                    value = """
+                                            {
+                                                 "id": "ac1d8001-8680-169d-8186-8017ac5f0000",
+                                                 "userName": "Martinex",
+                                                 "avatar": "https://robohash.org/81.36.52.170.png",
+                                                 "nombre": "David",
+                                                 "apellido": "García María",
+                                                 "fechaCreacion": "2023-02-23",
+                                                 "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhYzFkODAwMS04NjgwLTE2OWQtODE4Ni04MDE3YWM1ZjAwMDAiLCJpYXQiOjE2NzcxODYyODgsImV4cCI6MTY3NzI3MjY4OH0.t6CNlftomn0jwfrqNRt4bWCNf4FOLF_exDzSFSgxuMKd4NvIOROtRlBWEwvurlV1bUgVI66TpLd36E8I9fTkxA"
+                                             }
+                                            """
+                            )})}),
+            @ApiResponse(responseCode = "401",
+                    description = "Las credenciales que se han introducido son erróneas",
+                    content = @Content)
+    })
     @PostMapping("/auth/login")
     public ResponseEntity<JwtUserResponse> login(@RequestBody LoginRequest loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
@@ -65,14 +192,14 @@ public class UserController {
 
     @PutMapping("/user/changePassword")
     public ResponseEntity<CrearUsuarioResponse> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest,
-                                                       @AuthenticationPrincipal Usuario loggedUser) {
-    try {
+                                                               @AuthenticationPrincipal Usuario loggedUser) {
+        try {
             if (usuarioService.passwordMatch(loggedUser, changePasswordRequest.getOldPassword())) {
                 Optional<Usuario> modified = usuarioService.editPassword(loggedUser.getId(), changePasswordRequest.getNewPassword());
                 if (modified.isPresent())
                     return ResponseEntity.ok(CrearUsuarioResponse.fromUsuario(modified.get()));
             } else {
-                 throw new RuntimeException();
+                throw new RuntimeException();
             }
         } catch (RuntimeException ex) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password Data Error");
@@ -82,7 +209,7 @@ public class UserController {
     }
 
     @GetMapping("/me")
-    public CrearUsuarioResponse profile(@AuthenticationPrincipal Usuario loggedUser){
+    public CrearUsuarioResponse profile(@AuthenticationPrincipal Usuario loggedUser) {
         return CrearUsuarioResponse.fromUsuario(loggedUser);
     }
 }
